2cd92758ede11ad02acb0f4d880588de6601d342
Sergey-Akmalov <akmolgt@gmail.com>
2017-08-29 17:34:10 +0300

    add editing function and test for it

--- test/controllers/driver_controller_test.js
+++ test/controllers/driver_controller_test.js
@@ -6,17 +6,34 @@ const app = require('../../app');
 const Driver = mongoose.model('driver');
 
 describe('Drivers controller', () => {
-    it('Post to /api/drivers creates a new driver', done => {
-        Driver.count().then(count => {
-            request(app)
-                .post('/api/drivers')
-                .send({ email: 'test@test.com' })
-                .end(() => {
-                    Driver.count().then(newCount => {
-                       assert(count + 1 === newCount);
-                        done();
-                    });
-                })
+  it('Post to /api/drivers creates a new driver', done => {
+    Driver.count().then(count => {
+      request(app)
+        .post('/api/drivers')
+        .send({email: 'test@test.com'})
+        .end(() => {
+          Driver.count().then(newCount => {
+            assert(count + 1 === newCount);
+            done();
+          });
+        })
+    });
+  });
+
+  it('Put to /api/drivers/id edits an existing driver', done => {
+    const driver = new Driver({email: 't@t.com', driving: false});
+
+    driver.save().then(() => {
+      request(app)
+        .put(`/api/drivers/${driver._id}`)
+        .send({driving: true})
+        .end(() => {
+          Driver.findOne({ email: 't@t.com' })
+            .then(driver => {
+              assert(driver.driving === true);
+              done();
+            })
         });
     });
+  });
 });
